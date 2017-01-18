1d22b003ab8215a70feed16c0a228c090d9b4088
zshifrin <zach.shifrin@gmail.com>
2017-01-18 14:17:57 -0600

    devise saving registration data and profile linked properly

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}, controllers: { registrations: 'registrations' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
+  # devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}, controllers: { registrations: 'registrations' }
   resources :users, :only => [:show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

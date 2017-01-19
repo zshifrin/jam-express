6d3f2e03e1f3423410f4b95dc3b4747952d23a7e
Justin Tallant <jtallant07@gmail.com>
2017-01-19 14:01:22 -0600

    user attending concert method

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,8 @@ class User < ApplicationRecord
   has_many :rides
   has_and_belongs_to_many :concerts
   has_many :parties
+
+  def attending?(concert)
+    concert.users.include? self
+  end
 end
